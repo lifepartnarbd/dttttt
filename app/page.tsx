03ac8ad3242a9bf0ec2761ceead0767e0775b621
lifepartnarbd <lifepartnarbd@gmail.com>
2026-09-26 21:45:22 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/HeroSection';
 import HowItWorks from '@/components/HowItWorks';
+import StatsSection from '@/components/StatsSection';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <HowItWorks />
+      <StatsSection />
     </>
   );
 }
