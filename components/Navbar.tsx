05c9716ba9065df412aac1dd1211407f693ea644
lifepartnarbd <lifepartnarbd@gmail.com>
2026-09-26 21:47:16 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0">
             <Link href="/" className="text-2xl font-bold text-emerald-600">
-              Halal Matrimony
+              Life Partner BD
             </Link>
           </div>
 
